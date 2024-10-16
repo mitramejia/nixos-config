2234441c3a4f06b207a4bf50d1602543bc930876
mitramejia <mitra@arrofinance.com>
2024-10-16 17:42:24 -0400

    chore: update flake.lock with new dependencies
    Added several new dependencies including hyprland, aquamarine, hyprcursor, and more. Updated existing versions and added new inputs to flake.nix.

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    hyprland.url = "github:hyprwm/Hyprland";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     stylix.url = "github:danth/stylix/ed91a20c84a80a525780dcb5ea3387dddf6cd2de";
