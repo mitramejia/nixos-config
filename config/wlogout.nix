4a07e36ec14858ecd5a4f3bb216276111f0f62ac
mitramejia <mitra@arrofinance.com>
2024-12-23 00:33:57 -0400

    fix(config): remove unnecessary max-height property
    The `max-height` property in the `wlogout.nix` configuration was redundant and has been removed. This ensures a cleaner and more consistent styling behavior.

--- config/wlogout.nix
+++ config/wlogout.nix
@@ -52,7 +52,6 @@
       	background-color: rgba(12, 12, 12, 0.3);
       	border: 3px solid #${config.stylix.base16Scheme.base05};
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-        max-height: 1000px;
       }
       button:focus,
       button:active,
