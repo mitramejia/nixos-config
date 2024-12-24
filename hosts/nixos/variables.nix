be4ab5beca17749314a340924947a3e6815fb431
mitramejia <mitra@arrofinance.com>
2024-12-24 10:20:41 -0400

    feat(nixos): update default wallpaper path
        Changed the default wallpaper image to "pexels.jpg" to refresh the system's appearance. This simplifies customization by referencing the updated file path.

--- hosts/nixos/variables.nix
+++ hosts/nixos/variables.nix
@@ -19,7 +19,7 @@
   # Documentation: https://github.com/Alexays/Waybar
   clock24h = false; # Set to true for 24-hour format, false for 12-hour format
   theme = "Catppuccin-Mocha"; # Theme for Waybar
-  wallpaper_img = "/home/mitra/Pictures/Wallpapers/yosemite.png"; # Path to the wallpaper image
+  wallpaper_img = "/home/mitra/Pictures/Wallpapers/pexels.jpg"; # Path to the wallpaper image
 
   # Program Options
   # Set default applications for the system.
