c6d92fba28cc2b862095fa5525db48974da4b9f3
mitramejia <mitra@arrofinance.com>
2025-01-06 12:01:18 -0400

    refactor: reorganize and consolidate home-manager programs
    Moved all program configurations under a unified `programs` block for better structure and readability. Updated flake.lock to reflect the latest dependencies and revisions, ensuring improved compatibility and functionality.

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -47,86 +47,6 @@ in {
   # Add IdeaVim configuration for JetBrains IDE's
   home.file.".ideavimrc".source = ../../config/ideavim/.ideavimrc;
 
-  programs.fzf = {
-    enable = true;
-    defaultOptions = ["--color 16"];
-    enableZshIntegration = true;
-  };
-
-  # Install & Configure Git
-  programs.git = {
-    enable = true;
-    userName = "${gitUsername}";
-    userEmail = "${gitEmail}";
-    difftastic = {enable = true;};
-    extraConfig = {
-      push = {
-        autoSetupRemote = true;
-      };
-    };
-  };
-
-  programs.gh = {
-    enable = true;
-    settings = {
-      git_protocol = "ssh";
-      editor = "vim";
-    };
-  };
-
-  programs.jq.enable = true;
-
-  programs.tmux = {
-    enable = true;
-    shell = "${pkgs.zsh}/bin/zsh";
-    historyLimit = 1000000;
-    terminal = "tmux-256color";
-    keyMode = "vi";
-    newSession = true;
-    mouse = true;
-    baseIndex = 1;
-    disableConfirmationPrompt = true;
-    prefix = "C-Space";
-    extraConfig = ''
-      set -g pane-base-index 1
-      set-window-option -g pane-base-index 1
-
-      # keybindings
-      bind-key -T copy-mode-vi v send-keys -X begin-selection
-      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
-      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
-
-      bind - split-window -v -c "#{pane_current_path}"
-      bind | split-window -h -c "#{pane_current_path}"
-
-      bind h select-pane -L
-      bind j select-pane -D
-      bind k select-pane -U
-      bind l select-pane -R
-    '';
-
-    plugins = with pkgs; [
-      {
-        plugin = tmuxPlugins.catppuccin;
-        extraConfig = ''
-          set -g @catppuccin_flavour 'mocha'
-          set -g @catppuccin_window_tabs_enabled on
-          set -g @catppuccin_date_time "%H:%M"
-        '';
-      }
-      {
-        plugin = tmuxPlugins.continuum;
-        extraConfig = ''
-          set -g @continuum-restore 'on'
-          set -g @continuum-boot 'on'
-          set -g @continuum-save-interval '10'
-        '';
-      }
-      tmuxPlugins.better-mouse-mode
-      tmuxPlugins.yank
-    ];
-  };
-
   # Create XDG Dirs
   xdg = {
     userDirs = {
@@ -196,9 +116,36 @@ in {
     };
   };
 
-  programs.home-manager.enable = true;
-
   programs = {
+    home-manager.enable = true;
+
+    # Install & Configure Git
+    git = {
+      enable = true;
+      userName = "${gitUsername}";
+      userEmail = "${gitEmail}";
+      difftastic = {enable = true;};
+      extraConfig = {
+        push = {
+          autoSetupRemote = true;
+        };
+      };
+    };
+    gh = {
+      enable = true;
+      settings = {
+        git_protocol = "ssh";
+        editor = "vim";
+      };
+    };
+
+    jq.enable = true;
+
+    fzf = {
+      enable = true;
+      defaultOptions = ["--color 16"];
+      enableZshIntegration = true;
+    };
     btop = {
       enable = true;
       settings = {
@@ -231,6 +178,8 @@ in {
       package = pkgs.starship;
     };
 
+    direnv.enable = true;
+
     zsh = {
       enable = true;
       enableCompletion = true;
@@ -255,6 +204,57 @@ in {
         username = username;
         host = host;
       };
+    };
+
+    tmux = {
+      enable = true;
+      shell = "${pkgs.zsh}/bin/zsh";
+      historyLimit = 1000000;
+      terminal = "tmux-256color";
+      keyMode = "vi";
+      newSession = true;
+      mouse = true;
+      baseIndex = 1;
+      disableConfirmationPrompt = true;
+      prefix = "C-Space";
+      extraConfig = ''
+        set -g pane-base-index 1
+        set-window-option -g pane-base-index 1
+
+        # keybindings
+        bind-key -T copy-mode-vi v send-keys -X begin-selection
+        bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+        bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
+        bind - split-window -v -c "#{pane_current_path}"
+        bind | split-window -h -c "#{pane_current_path}"
+
+        bind h select-pane -L
+        bind j select-pane -D
+        bind k select-pane -U
+        bind l select-pane -R
+      '';
+
+      plugins = with pkgs; [
+        {
+          plugin = tmuxPlugins.catppuccin;
+          extraConfig = ''
+            set -g @catppuccin_flavour 'mocha'
+            set -g @catppuccin_window_tabs_enabled on
+            set -g @catppuccin_date_time "%H:%M"
+          '';
+        }
+        {
+          plugin = tmuxPlugins.continuum;
+          extraConfig = ''
+            set -g @continuum-restore 'on'
+            set -g @continuum-boot 'on'
+            set -g @continuum-save-interval '10'
+          '';
+        }
+        tmuxPlugins.better-mouse-mode
+        tmuxPlugins.yank
+      ];
     };
   };
 }
