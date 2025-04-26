991350b0cafdbcdaea8c364b02adb0e7ebf8f0fe
mitramejia <mitra@arrofinance.com>
2025-04-26 15:40:16 -0400

    feat(home): enhance configuration for bat and btop
    Enable and configure the 'bat' program with extra packages and pager settings. Update 'btop' to include GPU support, additional settings for visualization, and enhanced system info display.

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -114,6 +114,18 @@ in {
     };
   };
 
+  programs.bat = {
+    enable = true;
+    config = {
+      pager = "less -FR";
+    };
+    extraPackages = with pkgs.bat-extras; [
+      batman
+      batpipe
+      batgrep
+    ];
+  };
+
   programs = {
     home-manager.enable = true;
 
@@ -146,8 +158,22 @@ in {
     };
     btop = {
       enable = true;
+      package = pkgs.btop.override {
+        rocmSupport = true;
+        cudaSupport = true;
+      };
       settings = {
         vim_keys = true;
+        rounded_corners = true;
+        proc_tree = true;
+        show_gpu_info = "on";
+        show_uptime = true;
+        show_coretemp = true;
+        cpu_sensor = "auto";
+        show_disks = true;
+        only_physical = true;
+        io_mode = true;
+        io_graph_combined = false;
       };
     };
 
