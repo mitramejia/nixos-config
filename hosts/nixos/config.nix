a8dd9ab925bc17db6dc60f24f934d8c9f83ca5aa
mitramejia <mitra@arrofinance.com>
2025-06-12 09:22:55 -0400

    fix(nixos): remove obsolete gc configuration
    Removed the outdated garbage collection settings from `config.nix` to were no longer in use and did not align with the current setup, ensuring a cleaner and more maintainable configuration.

--- hosts/nixos/config.nix
+++ hosts/nixos/config.nix
@@ -220,11 +220,6 @@ in {
       substituters = ["https://hyprland.cachix.org"];
       trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-    };
   };
 
   # Virtualization / Containers
--- hosts/nixos/nh.nix
+++ hosts/nixos/nh.nix
@@ -11,5 +11,4 @@
     };
     flake = "/home/${username}/nix-config";
   };
-
 }
