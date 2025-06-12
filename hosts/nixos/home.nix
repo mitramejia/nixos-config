e352ecfa0325d7cd0e9cc3771900533ecfe7288f
mitramejia <mitra@arrofinance.com>
2025-06-11 23:38:20 -0400

    fix(home): update kitty settings with improved customization
    Refined the Kitty terminal configuration in `home.nix` to enhance usability and customization. Changes include enabling new settings for tab styles, cursor behavior, font size adjustments, and clipboard mappings for improved workflow efficiency and user experience.

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -187,18 +187,31 @@ in {
       package = pkgs.kitty;
       shellIntegration.enableZshIntegration = true;
       settings = {
+        font_size = 12;
         scrollback_lines = 2000;
         wheel_scroll_min_lines = 1;
         window_padding_width = 4;
         confirm_os_window_close = 0;
+        enable_audio_bell = false;
+        mouse_hide_wait = 60;
+        cursor_trail = 1;
+        tab_fade = 1;
+        active_tab_font_style = "bold";
+        inactive_tab_font_style = "bold";
+        tab_bar_edge = "top";
+        tab_bar_margin_width = 0;
+        tab_bar_style = "powerline";
+        enabled_layouts = "splits";
       };
       themeFile = "Catppuccin-Mocha";
       extraConfig = ''
-        tab_bar_style fade
-        tab_fade 1
-        font_size 11.5
-        active_tab_font_style   bold
-        inactive_tab_font_style bold
+        # Clipboard
+        map ctrl+shift+v        paste_from_selection
+        map shift+insert        paste_from_selection
+        # Miscellaneous
+        map ctrl+shift+up      increase_font_size
+        map ctrl+shift+down    decrease_font_size
+        map ctrl+shift+backspace restore_font_size
       '';
     };
 
