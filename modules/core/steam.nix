f6bfe394463b0b3491b09c24d1aab7f344cc07f9
mitramejia <mitra@arrofinance.com>
2025-11-28 12:16:20 -0400

    steam: open firewall for Local Network Game Transfers
    Enable programs.steam.localNetworkGameTransfers.openFirewall to allow peer-to-peer installs/updates over LAN. Remote Play and gamescope settings unchanged.

--- modules/core/steam.nix
+++ modules/core/steam.nix
@@ -6,8 +6,9 @@
       dedicatedServer.openFirewall = false;
       gamescopeSession.enable = true;
       extraCompatPackages = [pkgs.proton-ge-bin];
+       localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
     };
-
+    hardware.xone.enable = true; # support for the xbox controller USB dongle
     gamescope = {
       enable = true;
       capSysNice = true;
