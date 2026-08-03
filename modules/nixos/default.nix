a7a743c5c756a821504e035af0bc4f0e044adcb0
mitramejia <mitra@arrofinance.com>
2026-08-03 11:49:18 -0400

    chore(nix): tune nix daemon settings
    Raise download-attempts, http-connections, max-jobs, and
    max-substitution-jobs for faster/more resilient builds.

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -112,7 +112,11 @@ in {
 
   nix.settings = {
     auto-optimise-store = true;
+    download-attempts = 10;
     experimental-features = ["nix-command" "flakes"];
+    http-connections = 12;
+    max-jobs = 12;
+    max-substitution-jobs = 12;
     substituters = ["https://hyprland.cachix.org"];
     trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
   };
